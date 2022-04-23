d92dec5829dea65aed10a8a877970198e0d29ace
Juan Davalos <jcharlied13@gmail.com>
2022-04-23 00:06:08 -0700

    music stopped before restarting game
    soft bgm has been stopped before ending then

--- Assets/Scripts/Behaviors/RestartGame.cs
+++ Assets/Scripts/Behaviors/RestartGame.cs
@@ -6,6 +6,8 @@ public class RestartGame : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        AudioManager.SharedInstance.Stop(Constants.SoftBGM);
+        AudioManager.SharedInstance.Play(Constants.BGM, true);
         SceneManager.LoadScene("IntroScene");
     }
 
@@ -16,9 +18,9 @@ public class RestartGame : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-    }
+   // override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+   // {
+   // }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
